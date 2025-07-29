81dff5edd357140614d28d6d61b092c2845ab821
sanjana29207 <sanjana.a@s.amity.edu>
2025-07-29 12:08:39 +0530

    added celsius in the file

--- string.py
+++ string.py
@@ -4,3 +4,5 @@ celcius = float(input("enter temperature in celsius "))
 fahrenheit = (celsius * 9/5) + 32
 kelvin = celsius + 273.15
 printf("fahrenheit:",fahrenheit)
+if celsius > 30:
+    print("its hot!")
