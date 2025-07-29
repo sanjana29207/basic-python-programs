226f77ee354b67c286c27971a4b1bfcad053b918
sanjana29207 <sanjana.a@s.amity.edu>
2025-07-29 12:25:26 +0530

    deleted celsius in the file

--- string.py
+++ string.py
@@ -2,9 +2,9 @@ python
 
 celcius = float(input("enter temperature in celsius "))
 fahrenheit = (celsius * 9/5) + 22
-kelvin = celsius + 556.72
+kelvin = celse + 556.72
 printf("fahrenheit:",fahrenheit)
-if celsius > 20:
+if  celse  > 20:
     print("its hot!")
 else:
     printf("its cool!")
