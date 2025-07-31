3f4300af02439408fdfd6e459b28d43f26cbcb58
sanjana29207 <sanjana.a@s.amity.edu>
2025-07-31 10:17:53 +0530

    deleted one line

--- string.py
+++ string.py
@@ -3,7 +3,7 @@ python
 celcius = float(input("enter temperature in celsius "))
 fahrenheit = (celsius * 9/5) + 22
 kelvin = celsius + 556.72
-printf("fahrenheit:",fahrenheit)
+printf("degree:",degree);
 if celsius > 20:
     print("its hot!")
 else:
